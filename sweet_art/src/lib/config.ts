679df568bd9ced2fc5d74d580c63142b6d3eed2a
junyoungkim <juny3738@gmail.com>
2023-05-06 15:16:40 +0900

    sweet_art directory

--- sweet_art/src/lib/config.ts
+++
@@ -1,5 +0,0 @@
-import { getSiteConfig } from "./get-config-value"
-import { SiteConfig } from "./site-config"
-export const webTitle:SiteConfig['webTitle'] = getSiteConfig('webTitle','No Title')
-export const webDescription:SiteConfig['webDescription'] = getSiteConfig('webDescription','No Web Description')
-export const tabMenus:SiteConfig['tabMenus'] = getSiteConfig('tabMenus',[])
--- sweet_art/src/lib/types.ts
+++
@@ -1,6 +0,0 @@
-export type TabMenuType = 'notion'
-export interface TabMenu {
-	name : string
-	herf : string
-	// type : TabMenuType
-}
